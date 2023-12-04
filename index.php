c221b7c2f2a28b7cb121e62d0eaf24c7f34ba635
tetat <nishat00101@gmail.com>
2023-12-05 00:09:13 +0600

    User friendly modification

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ class DataHandling {
     protected string $Category = "";
 
     public function getInput($income_or_expense) {
+        // Id will start from 1. If income/expense file contain 2 exist income/expense then new income/expense Id will be 3.
         $this->Id = (int) (count(file("{$income_or_expense}.txt"))) + 1;
         $this->Amount = (int) (readline("Enter Amount: "));
         $this->Category = (string) (readline("Enter Category: "));
@@ -30,9 +31,7 @@ class AddData extends DataHandling{
         $this->getInput(IncomeExpense::INCOME);
 
         $file_pointer = fopen("income.txt", "a");
-
         fwrite($file_pointer, "{$this->Id} {$this->Amount} {$this->Category}\n");
-
         fclose($file_pointer);
 
         echo "Income added successfully.\n\n";
@@ -57,34 +56,30 @@ class ReadData {
     // protected int $total = 0;
     protected $incomes = [];
     protected $expenses = [];
-
+    // read all incomes from income file and save to incomes array.
     public function readIncome() {
         $file_pointer = fopen("income.txt", "r");
         while (true) {
             $income = fgets($file_pointer);
             if (!$income)break;
-            // echo $income;
             array_push($this->incomes, $income);
         }
         fclose($file_pointer);
-        // var_dump($this->incomes);
     }
-
+    // read all expenses from expense file and save to expenses array.
     public function readExpense() {
         $file_pointer = fopen("expense.txt", "r");
         while (true) {
             $expense = fgets($file_pointer);
             if (!$expense)break;
-            // echo $income;
             array_push($this->expenses, $expense);
         }
         fclose($file_pointer);
-        // var_dump($this->expenses);
     }
-
+    // readTotal function to find current total
     public function readTotal(): int {
-        $this->readIncome();
-        $this->readExpense();
+        $this->readIncome(); // make sure incomes array is not empty.
+        $this->readExpense(); // make sure expenses array is not empty.
 
         $total_income = (int) 0;
         foreach ($this->incomes as $income) {
@@ -97,32 +92,30 @@ class ReadData {
             $amount = (int) (explode(" ", $expense)[1]);
             $total_expense += $amount;
         }
-        // var_dump($total_income, $total_expense);
+        
         return $total_income - $total_expense;
     }
 }
 
 class ViewData extends ReadData {
     public function viewIncome() {
-        $this->readIncome();
+        $this->readIncome(); // make sure incomes array is not empty.
         
         echo "\nAll incomes:\n------------\n";
         echo "Id Amount Category\n";
         foreach ($this->incomes as $income) {
             echo $income;
         }printf("\n\n");
-        // var_dump($this->incomes);
     }
 
     public function viewExpense() {
-        $this->readExpense();
+        $this->readExpense(); // make sure expenses array is not empty.
 
         echo "\nAll expenses:\n-------------\n";
         echo "Id Amount Category\n";
         foreach ($this->expenses as $expense) {
             echo $expense;
         }printf("\n\n");
-        // var_dump($this->expenses);
     }
 
     public function viewTotal() {
@@ -130,8 +123,8 @@ class ViewData extends ReadData {
     }
 
     public function viewCategory() {
-        $this->readIncome();
-        $this->readExpense();
+        $this->readIncome(); // make sure incomes array is not empty.
+        $this->readExpense(); // make sure expenses array is not empty.
 
         echo "\nAll category from incomes:\n--------------------------\n";
         foreach ($this->incomes as $income) {
@@ -146,7 +139,7 @@ class ViewData extends ReadData {
 }
 
 class UpdateData extends ReadData {
-    private function reWriteIncomes() {
+    private function reWriteIncomes() { // function to update income file.
         $file_pointer = fopen("income.txt", "w");
 
         foreach ($this->incomes as $income) {
@@ -156,7 +149,7 @@ class UpdateData extends ReadData {
         fclose($file_pointer);
     }
 
-    private function reWriteExpense() {
+    private function reWriteExpense() { // function to update expense file.
         $file_pointer = fopen("expense.txt", "w");
 
         foreach ($this->expenses as $expense) {
@@ -167,14 +160,15 @@ class UpdateData extends ReadData {
     }
 
     public function updateIncome() {
+        // new Amount and Category for update.
         $update_income = (new DataHandling())->getInputForUpdate(IncomeExpense::INCOME);
 
-        $this->readIncome();
-        // var_dump($update_income);
+        $this->readIncome(); // added previous incomes to incomes array.
+        
         $update = false;
         for ($i = 0; $i < count($this->incomes); $i++) {
             $previous_income = explode(" ", $this->incomes[$i]);
-            // var_dump($update_income, $previous_income);
+            
             if ($previous_income[0] === $update_income[0]) {
                 $Id = $previous_income[0];
                 $Amount = ($update_income[1] === "" ? $previous_income[1] : $update_income[1]);
@@ -193,14 +187,15 @@ class UpdateData extends ReadData {
     }
 
     public function updateExpense() {
+        // new Amount and Category for update.
         $update_expense = (new DataHandling())->getInputForUpdate(IncomeExpense::EXPENSE);
 
-        $this->readExpense();
-        // var_dump($update_expense);
+        $this->readExpense(); // added previous expenses to expenses array.
+        
         $update = false;
         for ($i = 0; $i < count($this->expenses); $i++) {
             $previous_expense = explode(" ", $this->expenses[$i]);
-            // var_dump($update_expense, $previous_expense);
+            
             if ($previous_expense[0] === $update_expense[0]) {
                 $Id = $previous_expense[0];
                 $Amount = ($update_expense[1] === "" ? $previous_expense[1] : $update_expense[1]);
@@ -218,54 +213,72 @@ class UpdateData extends ReadData {
     }
 }
 
+// main code start
 while (true) {
-    echo "Type a number between 0 - 3 for which option you want...\n";
+    echo "Main menu.\n";
     echo "1 for add Income/Expense.\n";
     echo "2 for view Income/Expense/Total/Category.\n";
     echo "3 for update Income/Expense.\n";
     echo "0 for Close.\n";
     $primary_option = (int) readline("Enter a number: ");
 
-    if ($primary_option === 0) break;
+    if ($primary_option === 0) {
+        break;
+    }
     else if ($primary_option === 1) {
-        echo "\n1 for add an Income.\n";
-        echo "2 for add an Expense.\n";
-        $secondary_option = (int) readline("Enter a number: ");
+        while (true) {
+            echo "\n1 for add an Income.\n";
+            echo "2 for add an Expense.\n";
+            echo "0 for back to Main menu.\n";
+            $secondary_option = (int) readline("Enter a number: ");
 
-        $addData = new AddData();
+            $addData = new AddData();
 
-        if ($secondary_option === 1) $addData->addIncome();
-        else if ($secondary_option === 2) {
-            $total = (new ReadData())->readTotal();
-            // var_dump($total);
-            $addData->addExpense($total);
-        } else echo "Invalid input!\n\n";
+            if ($secondary_option === 0) break;
+            else if ($secondary_option === 1) $addData->addIncome();
+            else if ($secondary_option === 2) {
+                $total = (new ReadData())->readTotal();
+                $addData->addExpense($total);
+            }
+            else echo "Invalid input!\n\n";
+        }
+        printf("\n\n");
     }
     else if ($primary_option === 2) {
-        echo "\n1 for view Income.\n";
-        echo "2 for view Expense.\n";
-        echo "3 for view Total money.\n";
-        echo "4 for view Category.\n";
-        $secondary_option = (int) readline("Enter a number: ");
+        while (true) {
+            echo "\n1 for view Income.\n";
+            echo "2 for view Expense.\n";
+            echo "3 for view Total money.\n";
+            echo "4 for view Category.\n";
+            echo "0 for back to Main menu.\n";
+            $secondary_option = (int) readline("Enter a number: ");
 
-        $viewData = new ViewData();
+            $viewData = new ViewData();
 
-        if ($secondary_option === 1) $viewData->viewIncome();
-        else if ($secondary_option === 2) $viewData->viewExpense();
-        else if ($secondary_option === 3) $viewData->viewTotal();
-        else if ($secondary_option === 4) $viewData->viewCategory();
-        else echo "Invalid input!\n\n";
+            if ($secondary_option === 0) break;
+            else if ($secondary_option === 1) $viewData->viewIncome();
+            else if ($secondary_option === 2) $viewData->viewExpense();
+            else if ($secondary_option === 3) $viewData->viewTotal();
+            else if ($secondary_option === 4) $viewData->viewCategory();
+            else echo "Invalid input!\n\n";
+        }
+        printf("\n\n");
     }
     else if ($primary_option === 3) {
-        echo "\n1 for update an Income.\n";
-        echo "2 for update an Expense.\n";
-        $secondary_option = (int) readline("Enter a number: ");
+        while (true) {
+            echo "\n1 for update an Income.\n";
+            echo "2 for update an Expense.\n";
+            echo "0 for back to Main menu.\n";
+            $secondary_option = (int) readline("Enter a number: ");
 
-        $updateData = new UpdateData();
+            $updateData = new UpdateData();
 
-        if ($secondary_option === 1) $updateData->updateIncome();
-        else if ($secondary_option === 2) $updateData->updateExpense();
-        else echo "Invalid input!\n\n";
+            if ($secondary_option === 0) break;
+            else if ($secondary_option === 1) $updateData->updateIncome();
+            else if ($secondary_option === 2) $updateData->updateExpense();
+            else echo "Invalid input!\n\n";
+        }
+        printf("\n\n");
     }
     else {
         echo "Invalid input!\n\n";
